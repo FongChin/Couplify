f810422cf3a5c433330fa79cd3125757f8375515
Fong Chin <moon1991@gmail.com>
2014-01-16 10:47:04 -0800

    verify sendgrid acc

--- config/environment.rb
+++ config/environment.rb
@@ -6,17 +6,26 @@ Couplify::Application.initialize!
 
 if Rails.env.production?
   # only send real emails in production; use Sengrid
-  # ActionMailer::Base.smtp_settings = {
-  #   :address        => 'smtp.sendgrid.net',
-  #   :port           => '587',
-  #   :authentication => :plain,
-  #   :user_name      => ENV['SENDGRID_USERNAME'],
-  #   :password       => ENV['SENDGRID_PASSWORD'],
-  #   :domain         => 'heroku.com'
-  # }
-  # ActionMailer::Base.delivery_method ||= :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method ||= :smtp
 elsif Rails.env.development?
   # Remember the letter_opener gem? It won't send real emails; it
   # just opens them in another tab.
-  ActionMailer::Base.delivery_method = :letter_opener
+  # ActionMailer::Base.delivery_method = :letter_opener
+  ActionMailer::Base.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '587',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com'
+  }
+  ActionMailer::Base.delivery_method ||= :smtp
 end
