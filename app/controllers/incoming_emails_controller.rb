daf2c64aa504e6b667063b22a1ec7280445179e1
Fong Chin <moon1991@gmail.com>
2014-01-17 14:00:49 -0800

    print out parts of params in incoming_emails#create

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -2,16 +2,20 @@ class IncomingEmailsController < ApplicationController
   
   def create
     printa params["envelope"]
-    sender_id = User.get_id_from_email(params["envelope"]["from"])
-    couple_id = Couple.get_couple_id(params["to"])
-    printa sender_id
-    printa couple_id
-    body = params["text"]
-    if params["attachments"] > 0
-      params["attachment-info"].each do |attachment| do
-        printa attachment
-      end
-    end
+    printa params["to"]
+    printa params["text"]
+    printa params["attachments"]
+    printa params["attachment-info"]
+    # sender_id = User.get_id_from_email(params["envelope"]["from"])
+    #     couple_id = Couple.get_couple_id(params["to"])
+    #     printa sender_id
+    #     printa couple_id
+    #     body = params["text"]
+    #     if params["attachments"] > 0
+    #       params["attachment-info"].each do |attachment| do
+    #         printa attachment
+    #       end
+    #     end
   end
   
   def printa(a)
