ee6e94b978381688e3387fcea2410f1cc9aa3615
Fong Chin <moon1991@gmail.com>
2014-01-17 17:30:44 -0800

    testing to see if user_id is valid

--- app/controllers/incoming_emails_controller.rb
+++ app/controllers/incoming_emails_controller.rb
@@ -4,39 +4,43 @@ class IncomingEmailsController < ApplicationController
     printa "action received"
     sender_id = User.get_id_from_email(params["from"])
     couple_id = Couple.get_couple_id(params["to"])
-    body = params["text"]
-    attachment = params["attachment1"]
-    if attachment
-      printa attachment
-      printa attachment.tempfile.to_path.to_s
-      img = Magick::ImageList.new(attachment.tempfile.to_path.to_s)
-      s3 = AWS::S3.new(:access_key_id => ENV['AMAZONS3_KEY_ID'], :secret_access_key => ENV['AMAZONS3_SECRET_ACCESS_KEY'])
+    if sender_id && couple_id
+      body = params["text"]
+      attachment = params["attachment1"]
+      if attachment
+        printa attachment
+        printa attachment.tempfile.to_path.to_s
+        img = Magick::ImageList.new(attachment.tempfile.to_path.to_s)
+        s3 = AWS::S3.new(:access_key_id => ENV['AMAZONS3_KEY_ID'], :secret_access_key => ENV['AMAZONS3_SECRET_ACCESS_KEY'])
       
-      p "filename"
-      filename = attachment.original_filename.split(" ").join("_")
-      printa filename
+        p "filename"
+        filename = attachment.original_filename.split(" ").join("_")
+        printa filename
       
-      key = "couples/#{couple_id}/#{filename}"
-      s3_img = s3.buckets["couplify-development"].objects[key].write(
-        img.to_blob, {:acl => :public_read}
-      )
-      img_url = "https://s3-us-west-1.amazonaws.com/couplify-development/#{s3_img.key}"
+        key = "couples/#{couple_id}/#{filename}"
+        s3_img = s3.buckets["couplify-development"].objects[key].write(
+          img.to_blob, {:acl => :public_read}
+        )
+        img_url = "https://s3-us-west-1.amazonaws.com/couplify-development/#{s3_img.key}"
       
-      p "image url is"
-      printa img_url
-      printa sender_id
-      printa couple_id
+        p "image url is"
+        printa img_url
+        printa "sender_id is#{sender_id}"
+        printa couple_id
       
-      msg = Message.create(
-        :couple_id => couple_id,
-        :user_id => sender_id,
-        :body => body,
-        :image_url => img_url
-      )
-      msg.save!
+        msg = Message.new(
+          :couple_id => couple_id,
+          :user_id => sender_id,
+          :body => body,
+          :image_url => img_url
+        )
+        if msg.save
+          render :text => "success"
+        end
+      end
+      
     end
-    
-    render :text => "success"
+    render :text => "..."
   end
   
   def printa(a)
