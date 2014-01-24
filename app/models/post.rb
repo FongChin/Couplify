1ad9bafeead8049ea6f72e25b4871519a9376646
Fong Chin <moon1991@gmail.com>
2014-01-24 14:25:10 -0800

    testing auto-orient again

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
   
   has_attached_file :image, 
                     :styles => { :medium => "500x500>" }, 
-                    :processors => [:no_rotation]
+                    :cover_options => {:all => '-auto-orient'}
   
   def image_url
     (self.image_file_name.nil?) ? nil : self.image.url
